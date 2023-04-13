82a53b3c8fd965a5e2ba3f406905ad0d2b36ee01
wayne1512 <57416683+wayne1512@users.noreply.github.com>
2023-04-13 22:09:31 +0200

    BNF

--- src/main/java/frontend/parseRules/ProgramParseRule.java
+++ src/main/java/frontend/parseRules/ProgramParseRule.java
@@ -4,16 +4,17 @@ import exceptions.SyntaxErrorException;
 import frontend.ParserContext;
 import frontend.ast.ASTNode;
 import frontend.ast.BlockAstNode;
+import frontend.ast.ProgramAstNode;
 import frontend.ast.StatementListAstNode;
 import frontend.tokens.Token;
 
-public class ProgramParseRule implements ParseRule<ASTNode>{
+public class ProgramParseRule implements ParseRule<ProgramAstNode>{
 
     @Override
-    public ASTNode parse(ParserContext pc) throws SyntaxErrorException{
+    public ProgramAstNode parse(ParserContext pc) throws SyntaxErrorException{
 
         StatementListAstNode child = new StatementListParseRule().parse(pc);
 
-        return new BlockAstNode(child.getSourceStart(),child.getSourceEnd(),child);
+        return new ProgramAstNode(child.getSourceStart(),child.getSourceEnd(),child);
     }
 }
